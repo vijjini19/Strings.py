52dc43df969616168a5437e24d1e1e0731e44c56
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-06-17 21:34:27 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,46 @@
+# print(type(''))
+# print(type(""))
+# print(type(''''''))
+# print(type(""""""))
+
+# print(type('45'))
+# print(type("5.6"))
+# print(type('''45tu'''))
+# print(type(","))
+
+# print(type(0))
+
+# n='sindhu'
+# p='hyderabad'
+# s='btech'
+# a=24
+# print('my name is',n,'from',p,'my age is',a,'i complted',s)
+
+# a=34 ; b=4.5 ; c='hey'
+# print(a+b) #38.5
+# print(b+a) #38.5
+# print(a+c) #TypeError
+# print(b+c) #TypeError
+
+# x='hey' ; y='hi'
+# print(x+y) #heyhi
+# print(y+x) #hihey
+
+# print('hello world')
+# print('hello','world')
+# print('hello ','world')
+# print('hello',' world')
+# print('hello'+' '+'world')
+
+# print(''+'hi')
+# print(' '+'hi')
+
+# print(''.isspace())
+# print(' '.isspace())
+# print(','.isspace())
+# print('hey'.isspace())
+
+# print('wed','sat')
+# print('wed'+""+'sat')
+# print('wed'+'  '+'sat')
+
